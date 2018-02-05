ac96e5e573c07fdfe8fd8a52a4304d263c0b48aa
Felipe Martins <me@fefas.net>
2018-02-05 22:45:27 +0100

    Add test specific to verify item existence

---
+++ tests/VerifyIfConfigsExistInContainerTest.php
@@ -0,0 +1,52 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Bauhaus\Config;
+
+class VerifyIfItemExistsInContainerTest extends TestCase
+{
+    private $config;
+
+    protected function setUp(): void
+    {
+        $this->config = new Config([
+            'fefas' => [
+                'pokemon' => 'charmander',
+            ],
+        ]);
+    }
+
+    /**
+     * @test
+     * @dataProvider existingLabels
+     */
+    public function returnTrueIfThereIsItemWithTheGivenLabel(string $label)
+    {
+        $this->assertTrue($this->config->has($label));
+    }
+
+    public function existingLabels(): array
+    {
+        return [
+            'Simple label' => ['fefas'],
+            'Composed label' => ['fefas.pokemon'],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider nonExistingLabels
+     */
+    public function returnFalseIfThereIsNotItemWithTheGivenLabel(string $label)
+    {
+        $this->assertFalse($this->config->has($label));
+    }
+
+    public function nonExistingLabels(): array
+    {
+        return [
+            'Simple label' => ['safef'],
+            'Composed label' => ['fefas.intruments'],
+        ];
+    }
+}
