675b225b2f1492ca3a35a29ff35cb266bcc8ab04
Felipe Martins <me@fefas.net>
2018-02-11 00:27:43 +0100

    Add test to ensure objects can't be added to Config

---
+++ tests/DoNotAcceptObjectValuesTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Bauhaus;
+
+use StdClass;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+class DoNotAcceptObjectValuesTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function throwInvalidArgumentExceptionIfGivenValueIsAnObject()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("Config 'key' is an object");
+
+        $config = new Config([
+            'key' => new StdClass(),
+        ]);
+    }
+}
